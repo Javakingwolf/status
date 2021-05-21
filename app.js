d7916c7ae4bd56aff0e09431d94604c2e45b9918
wolfyuri <37412793+Javakingwolf@users.noreply.github.com>
2021-05-21 15:31:29 +0430

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var wolfyuri = document.createElement('div');
+ wolfyuri.className = "server-n";
+ wolfyuri.style.cssText = "position: relative;-webkit-animation: colorchange 2s infinite alternate;margin-left: 4.5vw;width: 10.5vw;transition: all 0.3s ease 0s;height: 1.8vw;font-size: 1.2vw;color: white;text-align: center;letter-spacing: 0.1vw;border-left: 0.2vw solid RGBA(0,0,0,1);padding-left: 0.5vw;border-right: 0.2vw solid RGBA(0,0,0,1);border-radius: 0.5vw;background-color: RGBA(0,0,0,0.5);display: block !important;"
+ wolfyuri.innerHTML = "<span>MadGames</span>";
+ document.getElementById('player-header').appendChild(wolfyuri);
